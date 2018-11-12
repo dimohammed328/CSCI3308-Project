2a6456abab8efe56306b1564fba66d2aa8d0f087
ryanwood3 <ryan.wood-1@colorado.edu>
2018-11-12 15:24:49 -0700

    Basic Song Selection page and login authentication

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import bcrypt from "bcrypt-nodejs";
 import { Redirect } from "react-router-dom";
@@ -7,37 +8,64 @@ const axios = require("axios");
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
-  render() {
-    var onSubmit = e => {
-      e.preventDefault();
-      console.log(e.target[0].value);
-      console.log(e.target[1].value);
-      axios
-        .post("https://lyricrace-backend.herokuapp.com/login/", {
-          username: e.target[0].value,
-          password: e.target[1].value
-        })
-        .then(function(response) {
-          console.log("response: ", response);
-        })
-        .catch(function(error) {
-          console.log("why the fuck", error);
-        });
+    this.state = {
+      userName: "",
+      password: ""
     };
+
+    this.onUserChange = this.onUserChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onUserChange (e) {
+    this.setState({userName: e.target.value});
+  }
+  onPasswordChange (e) {
+    this.setState({password: e.target.value});
+  }
+
+  onSubmit(e) {
+    alert('A name was submitted: ' + this.state.userName);
+    alert('A password was submitted: ' + this.state.password);
+    e.preventDefault();
+    
+    axios
+      .post("https://lyricrace-backend.herokuapp.com/login/", {
+        username: e.target[0].value,
+        password: e.target[1].value
+      })
+      .then(res => {
+        const response = res.data.message;
+        if(response=="Successfully Logged In")
+        {
+          
+        }
+        else
+        {
+          alert(response);
+        }
+      })
+      .catch(function(error) {
+        console.log("why the fuck", error);
+      });
+    }
+
+  render() {
     return (
       <div className="loginRow">
         <div className="loginHolder">
-          <Form className="loginForm" onSubmit={onSubmit}>
+          <Form className="loginForm" onSubmit={this.onSubmit}>
             <h2>Log In</h2>
             <FormGroup>
-              <Label for="username">Test</Label>
+              <Label for="username">Username</Label>
               <Input
                 type="text"
                 name="username"
                 id="username"
                 placeholder="Username"
+                value={this.state.userName}
+                onChange={this.onUserChange}
               />
             </FormGroup>
             <FormGroup>
@@ -47,10 +75,12 @@ export default class LoginForm extends Component {
                 name="password"
                 id="password"
                 placeholder="Password"
+                value={this.state.password}
+                onChange={this.onPasswordChange}
               />
             </FormGroup>
-            <Button className="submitButton" color="primary">
-              Log In
+            <Button type="submit" className="submitButton" color="primary">
+                Log In
             </Button>
           </Form>
         </div>
---
+++ src/components/SongSelection.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar.js";
+
+export default class SongSelection extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Song Selection</h1>
+      </div>
+    );
+  }
+}
