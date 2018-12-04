cc0f439261c770d41c8dbf47aeb33e7b39e3e681
Escarose <43680632+Escarose@users.noreply.github.com>
2018-12-03 23:58:30 -0700

    leaderboard prototype - update desc sort
    need to find out how to get score and name input from the actual game and put inside the return function

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -4,12 +4,27 @@ import bcrypt from "bcrypt-nodejs";
 import { Redirect } from "react-router-dom";
 import "../styles/Leaderboard.css";
 const axios = require("axios");
+
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
+
+  document.addEventListener('DOMContentLoaded', () => {
+  let elements = []
+  let container = document.querySelector('#container')
+  // Add each row to the array
+  container.querySelectorAll('.row').forEach(el => elements.push(el))
+  // Clear the container
+  container.innerHTML = ''
+  // Sort the array from highest to lowest
+  elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
+  // Put the elements back into the container
+  elements.forEach(e => container.appendChild(e))
+  })
+
     return (
       <div id="container">
     <div class="row">
