5f2c21064c1edb7d00afb73cf0aab2d83cc74f44
Escarose <43680632+Escarose@users.noreply.github.com>
2018-12-06 13:28:31 -0700

    update link to leaderboard

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -43,7 +43,7 @@ export default class NavBar extends Component {
                 <Link to="/songselection" className="link">
                   Song Selection
                 </Link>
-                        </NavLink>
+              </NavLink>
             </NavItem>
              <NavItem className="navItem">
               <NavLink>
