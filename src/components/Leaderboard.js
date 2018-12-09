eec013939c8a8523eba0a6235c14a97ba1201617
Danish Mohammed <dimohammed328@gmail.com>
2018-12-08 16:49:00 -0800

    leaderboard finalization

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -11,56 +11,38 @@ export default class LoginForm extends Component {
     this.state = {};
   }
 
-componentDidMount() {
+  componentDidMount() {
     var list = [];
     axios
-        .get("https://lyricrace-backend.herokuapp.com/leaderboard/", {})
-        .then(res => {
-        console.log(res);
-        })
-        .catch(function(error) {
-        console.log("why the fuck", error);
+      .get("https://lyricrace-backend.herokuapp.com/leaderboard/")
+      .then(res => {
+        this.setState({
+          list: res.data.rows.sort((a, b) => b.timesec - a.timesec)
         });
-
-}
+        this.state.list.forEach((item, index) => {
+          console.log("hello \n\n\n", item);
+          item.key = index;
+        });
+        this.setState({
+          listHTML: this.state.list.map(item => (
+            <div key={item.key} className="row">
+              <div className="name">{item.username}</div>
+              <div classname="score">{item.timesec}</div>
+            </div>
+          ))
+        });
+        console.log(this.state.list);
+      })
+      .catch(function(error) {
+        console.log("why the fuck", error);
+      });
+  }
 
   render() {
-
-  document.addEventListener('DOMContentLoaded', () => {
-  let elements = []
-  let container = document.querySelector('#container')
-  // Add each row to the array
-  container.querySelectorAll('.row').forEach(el => elements.push(el))
-  // Clear the container
-  container.innerHTML = ''
-  // Sort the array from highest to lowest
-  elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
-  // Put the elements back into the container
-  elements.forEach(e => container.appendChild(e))
-  })
-
-    return (
-      <div id="container">
-    <div className="row">
-        <div className="name">Player1</div><div className="score">430</div>
-    </div>
-    
-    <div className="row">
-        <div className="name">Player2</div><div className="score">580</div>
-    </div>
-    
-    <div className="row">
-        <div className="name">Player3</div><div className="score">310</div>
-    </div>
-    
-    <div className="row">
-        <div className="name">Player4</div><div className="score">640</div>
-    </div>
-    
-    <div className="row">
-        <div className="name">Player5</div><div className="score">495</div>
-    </div>
-</div>
-    );
+    if (this.state.listHTML) {
+      return <div id="container">{this.state.listHTML}</div>;
+    } else {
+      return <h1 style={{ color: "white" }}>Loading...</h1>;
+    }
   }
 }
